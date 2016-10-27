b425bc3ff3112a7f9c824f333d0de60895fefa73
Jonathan <jonathanpham5@gmail.com>
2016-10-26 19:11:32 -0600

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+/*
+	Author: Jonathan Pham
+	File: main.cpp
+	Key Encryption
+*/
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+	std::string fileName = "";
+
+	// Command Line Arguments are 2: ./a.out <insert string here>
+	if (argc == 2) {
+		std::string cmdline(argv[1]);
+
+		std::cout << "You entered:\n";
+		std::cout << cmdline << std::endl;
+
+		// Encrypt String cmdline here
+	} else {
+		// Read File to encrypt contents of file
+		std::cout << "Hello, enter the file to be encrypted: ";
+		std::cin >> fileName;
+
+		std::ifstream file(fileName.c_str());
+		std::stringstream buffer;
+		buffer << file.rdbuf();
+
+		// buffer.str() has contents of the file as a string
+		std::cout << buffer.str() << std::endl;
+
+		// Encrypt buffer.str() here
+	}
+}
